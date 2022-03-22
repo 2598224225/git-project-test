0e13bf66ff41f90bae5af087330aafce9b61d277
fyw <2598224225@qq.com>
2022-03-22 11:07:15 +0800

    push commit

--- src/main/java/com/fan/git/GitTest.java
+++ src/main/java/com/fan/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello git4   !");
         System.out.println("hello git4   !");
         System.out.println("host-fix test!");
+        System.out.println("push test!");
 
     }
 }
