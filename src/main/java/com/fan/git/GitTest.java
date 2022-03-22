bc6a467460255a50145f35154cd6c2bea2dd090d
范一伟 <102020365+fyw2598@users.noreply.github.com>
2022-03-22 11:13:03 +0800

    pull test

--- src/main/java/com/fan/git/GitTest.java
+++ src/main/java/com/fan/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello git4   !");
         System.out.println("host-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
 
     }
 }
